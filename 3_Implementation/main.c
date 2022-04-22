f7534218661391026d34563e092df0262c3c6b22
Vaibhavi Musale <101445551+VaibhaviMusale4@users.noreply.github.com>
2022-04-22 18:04:40 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,30 @@
+#include "calculator.h"
+void reset_display(){
+	
+}
+int main(){
+  uint8_t key_press;
+  uint16_t result;
+
+  lcd_init();
+  keypad_init();
+	
+  
+  while(1){
+	   key_press=keypad_scan();
+	   if(key_press !=0){	
+	   
+		if(key_press=='c'){
+			lcd_send_command(LCD_CMD_CLEAR_DISPLAY);
+			lcd_write_word("Enter Values!");
+			lcd_goto_xy(1,0);
+			result=scan_and_eval();
+			print_result(result);
+			
+			}
+		}
+		
+   }
+
+  return 0;
+}
